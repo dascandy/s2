18d11f2da19985a8e6ed5a7e830907ede4dafc07
Peter Bindels <peter.bindels@tomtom.com>
2017-07-07 17:27:50 +0200

    Updated future implementation, started on vector

--- include/s2/detail/future_impl.h
+++ include/s2/detail/future_impl.h
@@ -6,6 +6,7 @@
 #include <atomic>
 #include <thread>
 #include <chrono>
+#include <iostream>
 
 using namespace std::literals::chrono_literals;
 namespace s2 {
@@ -13,77 +14,94 @@ namespace s2 {
 template <typename T, size_t N = 1024>
 class async_queue {
 public:
+  async_queue() {}
+  async_queue(const async_queue&) = delete;
+  async_queue(async_queue&&) = delete;
+  async_queue& operator=(const async_queue&) = delete;
+  async_queue& operator=(async_queue&&) = delete;
+  ~async_queue() {
+    while (read != write) {
+      try {
+        pop();
+      } catch (...) {}
+    }
+  }
   void push(T&& value) noexcept {
-    std::unique_lock<std::mutex> l(m);
-    wcv.wait(l, [this]{ return ((write + 1) % N) != read; });
-    size_t mine = write;
-    write = (write + 1) % N;
-    new (&storage()[mine])T(std::forward<decltype(value)>(value));
+    {
+      std::unique_lock<std::mutex> l(m);
+      wcv.wait(l, [this]{ return !full(); });
+      size_t mine = write;
+      write = (write + 1) % N;
+      new (&storage()[mine])T(std::forward<decltype(value)>(value));
+    }
     rcv.notify_one();
   }
-  T pop() noexcept {
+  T pop() {
     std::unique_lock<std::mutex> l(m);
-    rcv.wait(l, [this]{ return read != write; });
+    rcv.wait(l, [this]{ return stop || !empty(); });
+    if (empty()) throw std::exception();
     size_t mine = read;
-    read = (read + 1) % N;
     T value = std::move(storage()[mine]);
     storage()[mine].~T();
+    read = (read + 1) % N;
     wcv.notify_one();
     return value;
   }
+  void notify() {
+    {
+      std::unique_lock<std::mutex> l(m);
+      stop = true;
+    }
+    rcv.notify_all();
+  }
 private:
+  bool empty() const noexcept {
+    return read == write;
+  }
+  bool full() const noexcept {
+    return ((write+1)%N) == read;
+  }
   T* storage() { return reinterpret_cast<T*>(&storage_); }
   std::mutex m;
   std::condition_variable rcv;
   std::condition_variable wcv;
-  size_t read, write;
-  std::aligned_storage<sizeof(T) * N, alignof(T)> storage_;
-};
-
-class threadpool {
-public:
-  template <typename T>
-  threadpool(T&& func, size_t threadcount = std::thread::hardware_concurrency()) 
-  : threads(threadcount)
-  {
-    for (size_t n = 0; n < threadcount; n++)
-      threads.emplace(threads.end(), func);
-  }
-  size_t count() const { return threads.size(); }
-private:
-  std::vector<std::thread> threads;
+  size_t read = 0, write = 0;
+  bool stop = false;
+  std::aligned_storage_t<sizeof(T) * N, alignof(T)> storage_;
 };
 
 class default_executor {
 public:
   default_executor() 
-  : running(true)
-  , threads_active(0)
-  , pool_([this]{ run(); })
   {
-  }
-  ~default_executor() {
-    int threads = pool_.count();
-    running = false;
-    // Schedule N blank tasks to allow all to exit
-    for (size_t n = 0; n < threads; n++) {
-      schedule([]{});
+    for (size_t n = 0; n < std::thread::hardware_concurrency(); n++) {
+      threads.emplace_back([this]{ run(); });
     }
-    while (threads_active > 0) { std::this_thread::sleep_for(1ms); }
+  }
+  default_executor(const default_executor&) = delete;
+  default_executor(default_executor&&) = delete;
+  default_executor& operator=(const default_executor&) = delete;
+  default_executor& operator=(default_executor&&) = delete;
+  ~default_executor() {
+    q_.notify();
+    for (auto& t : threads) {
+      t.join();
+    }
   }
   void run() {
-    while (running) {
-      auto task = q_.pop();
-      task();
+    try {
+      while (1) {
+        auto task = q_.pop();
+        task();
+      }
+    } catch(const std::exception& e) {
     }
   }
   void schedule(std::function<void()>&& task) {
     q_.push(std::move(task));
   }
-  std::atomic<bool> running;
-  std::atomic<int> threads_active;
   async_queue<std::function<void()>> q_;
-  threadpool pool_;
+  std::vector<std::thread> threads;
 };
 
 default_executor& get_default_executor() {
@@ -156,12 +174,12 @@ public:
     state = Exception;
     cv.notify_all();
   }
-private:
   void wait(std::unique_lock<std::mutex>& l) const {
     if (state == Empty) {
       cv.wait(l, [this]{ return state != Empty; });
     }
   }
+private:
   mutable std::mutex m;
   mutable std::condition_variable cv;
   enum { Empty, Value, Exception } state = Empty;
@@ -169,6 +187,43 @@ private:
     std::aligned_storage<sizeof(T), alignof(T)> storage;
     std::exception_ptr eptr;
   };
+};
+
+template <>
+class future<void> {
+public:
+  using value_type = void;
+  constexpr future() noexcept : state(nullptr) {}
+  future(future&&) noexcept = default;
+  future& operator=(future&&) noexcept = default;
+  future(const future&) noexcept = default;
+  future& operator=(const future&) noexcept = default;
+
+  template <typename Ex, typename C>
+  auto then(Ex&& ex, C&& c);
+  template <typename C>
+  auto then(C&& c) { return (*this).then(get_default_executor(), ::std::forward<decltype(c)>(c)); }
+  bool ready() const noexcept;
+  bool valid() const noexcept { return state != nullptr; }
+  void get() const;
+private:
+  future(shared_state<std::tuple<>>* state) : state(state) {}
+  friend class promise<void>;
+  shared_state<std::tuple<>> *state;
+};
+
+template <>
+class promise<void> {
+public:
+  promise() noexcept;
+  future<void> get_future();
+  void set_value();
+  void set_exception(std::exception_ptr&& error);
+  template <typename F, typename... Args>
+  void run_async(F&& f, Args&&... args);
+private:
+  shared_state<std::tuple<>> *state;
+
 };
 
 template <typename... Ts>
@@ -188,11 +243,13 @@ bool future<Ts...>::ready() const noexcept {
   return state->isset(); 
 }
 
+void future<void>::get() const {
+  state->get();
+}
+
 template <typename... Ts>
-const typename future<Ts...>::value_type& future<Ts...>::get() const {
-  if constexpr (sizeof...(Ts) == 0) {
-    return;
-  } else if constexpr (sizeof...(Ts) == 1) {
+auto future<Ts...>::get() const {
+  if constexpr (sizeof...(Ts) == 1) {
     return std::get<0>(state->get());
   } else {
     return state->get();
@@ -200,9 +257,8 @@ const typename future<Ts...>::value_type& future<Ts...>::get() const {
 }
 
 template <typename... Ts>
-constexpr promise<Ts...>::promise() noexcept {
+promise<Ts...>::promise() noexcept {
   state = new shared_state<std::tuple<Ts...>>();
-  printf("%p\n", state);
 }
 
 template <typename... Ts>
@@ -221,17 +277,50 @@ void promise<Ts...>::set_exception(std::exception_ptr &&error) {
   state->set_error(std::forward<decltype(error)>(error));
 }
 
+promise<void>::promise() noexcept 
+: state(new shared_state<std::tuple<>>())
+{
+}
+
+future<void> promise<void>::get_future() {
+  return future<void>(state);
+}
+
+void promise<void>::set_value() {
+  state->set_value(std::tuple<>());
+}
+
+void promise<void>::set_exception(std::exception_ptr &&error) {
+  state->set_error(std::forward<decltype(error)>(error));
+}
+
+template <typename... Ts>
+template <typename F, typename... Args>
+void promise<Ts...>::run_async(F&& f, Args&&... args) {
+  try {
+    set_value(f(std::forward<decltype(args)>(args)...));
+  } catch (...) {
+    set_exception(std::current_exception());
+  }
+}
+
+template <typename F, typename... Args>
+void promise<void>::run_async(F&& f, Args&&... args) {
+  try {
+    f(std::forward(args)...);
+    set_value();
+  } catch (...) {
+    set_exception(std::current_exception());
+  }
+}
+
 template <typename Executor, typename F, typename... Args>
 future<decltype(std::declval<F>()(std::declval<Args>()...))> async(Executor&& exec, F&& f, Args&&... args) {
   using rv = decltype(std::declval<F>()(std::declval<Args>()...));
   promise<rv> p;
   future<rv> rvalue = p.get_future();
-  std::function<void()> task([=]() mutable {
-    try {
-      p.set_value(f(std::forward(args)...));
-    } catch (...) {
-      p.set_exception(std::current_exception());
-    }
+  std::function<void()> task([=, p = std::move(p)]() mutable {
+    p.run_async(f, std::forward<decltype(args)>(args)...);
   });
   exec.schedule(std::move(task));
   return rvalue;
--- tests/future/test_simple.cpp
+++ tests/future/test_simple.cpp
@@ -1,9 +1,19 @@
 #include "catch.hpp"
 #include <s2/future>
 
-TEST_CASE( "Can use a basic future", "[future]" ) {
+TEST_CASE("do nothing in the default executor", "[future]") {
+  s2::default_executor def;
+}
+
+TEST_CASE("Can use a basic future", "[future]") {
   s2::future<int> i = s2::async([]{ return 42; });
   REQUIRE(i.get() == 42);
 }
+
+TEST_CASE("Can use void future", "[future]") {
+  s2::future<void> i = s2::async([]{});
+  i.get();
+}
+
 
 
